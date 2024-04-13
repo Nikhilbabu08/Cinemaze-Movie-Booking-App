16aa3b4422a3f4584875ccdb912148f28fffb14f
Nikhil Babu <nikhilbabu80942@gmail.com>
2024-04-13 23:36:47 +0530

    cors change

--- server/app.js
+++ server/app.js
@@ -14,7 +14,13 @@ const app = express()
 
 dotenv.config();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use('/user', userRouter)
