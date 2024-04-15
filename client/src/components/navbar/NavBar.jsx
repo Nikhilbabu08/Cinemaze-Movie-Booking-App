4bb00d00ae15f7891dadc7dd5a3444951e524ce7
Nikhil Babu <nikhilbabu80942@gmail.com>
2024-04-15 21:19:44 +0530

    offcanvas setup

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,46 +1,49 @@
-import React, { useState } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import React from 'react'
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { adminLogout, userLogout } from '../../store/AuthSlice';
-import API_BASE_URL from './../../config';
+import API_BASE_URL from './../../config'
+
 
 const NavBar = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const admin = useSelector(store => store.auth.admin);
-  const user = useSelector(store => store.auth.user);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const admin = useSelector(store => store.auth.admin)
+  const user = useSelector(store => store.auth.user)
 
   const handleAdminLogout = () => {
     if (admin) {
       axios.post(`${API_BASE_URL}/admin/logout`, {}, {
         headers: { 'Authorization': `Bearer ${admin.token}` }
-      });
-      dispatch(adminLogout());
-      navigate('adminLogin');
+      })
+      dispatch(adminLogout())
+      navigate('adminLogin')
     }
-  };
+  }
 
   const handleUserLogout = () => {
     if (user) {
       axios.post(`${API_BASE_URL}/user/logout`, {}, {
         headers: { 'Authorization': `Bearer ${user.token}` }
-      });
-      dispatch(userLogout());
-      navigate('userLogin');
+      })
+      dispatch(userLogout())
+      navigate('userLogin')
     }
-  };
-
+  }
   return (
     <div>
-      <nav className="navbar navbar-dark navbar-expand-lg fixed-top" style={{ backgroundColor: "#154360", color: "#000" }}>
+      <nav className="navbar navbar-dark navbar-expand-lg fixed-top space-between " style={{ backgroundColor: "#154360 ", color: "#000" }}>
         <div className="container-fluid">
           <NavLink to={'/'} className="navbar-brand"><b><span style={{ color: "#ff5733" }}>CIN</span>E<span style={{ color: "#ff5733" }}>MAZ</span>E</b></NavLink>
-          <button className="navbar-toggler" type="button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className={`bg-gradient collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNavAltMarkup" style={{ position: 'absolute', right: '0', top: '100%', color: "#154360" }}>
+          <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><b><span style={{ color: "#ff5733" }}>CIN</span>E<span style={{ color: "#ff5733" }}>MAZ</span>E</b></h5>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
             <div className="navbar-nav">
               <NavLink to={'/'} className='nav-link'>Home</NavLink>
             </div>
@@ -68,7 +71,7 @@ const NavBar = () => {
         </div>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
